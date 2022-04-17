7160ad94792c5758f500989fa38324be78410d42
Arif Mehniyar <arif.mehniyar@gmail.com>
2022-04-17 15:58:05 +0300

    gradient

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@ export default {
 </script>
 <style lang="scss">
 body{
-  background: rgb(191,237,255);
-  background: radial-gradient(circle, rgba(191,237,255,0.3669879356127107) 0%, rgba(255,255,255,1) 52%, rgba(248,248,248,1) 100%);
+  background: rgb(255,255,255);
+  background: linear-gradient(90deg, rgba(255,255,255,1) 13%, rgba(156,227,255,0.6815946771857444) 37%, rgba(156,227,255,1) 63%, rgba(248,248,248,1) 84%);
   scrollbar-width: thin;
   scrollbar-color: #ccc;
 
