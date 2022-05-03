3ada5aa10c0f7a593c9a694c51467c0b53bc0e79
Arif Mehniyar <arif.mehniyar@gmail.com>
2022-05-03 23:06:05 +0300

    pwa

--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,5 @@ module.exports = defineConfig({
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
 
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
   }
 })
