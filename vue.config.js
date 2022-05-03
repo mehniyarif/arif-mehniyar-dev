0d33ab1a1441f5741d21226db54073d460070d8c
Arif Mehniyar <arif.mehniyar@gmail.com>
2022-05-03 21:50:25 +0300

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Arif Mehniyar',
+    themeColor: '#9CE3FFAD',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 })
